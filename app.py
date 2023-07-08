2a9c624bf3e202e5bff726734c3aa2dd77034285
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-09 00:09:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
 import streamlit as st
 import requests
+from PIL import Image
+import io
 
 API_URL = "https://api-inference.huggingface.co/models/to-be/donut-base-finetuned-invoices"
 headers = {"Authorization": "Bearer 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"}
 
-def query(file):
-    response = requests.post(API_URL, headers=headers, data=file)
+def query(image_bytes):
+    response = requests.post(API_URL, headers=headers, data=image_bytes)
     return response.text
 
 def main():
     st.title("Documented Form Text Extraction")
     st.write("Upload an image of a handwritten form to extract the text.")
-    
+
+    sample_images = [
+        "image.jpg",
+        "image2.jpg",
+        "image3.jpg"
+    ]
+
     uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
-    
+
     if uploaded_file is not None:
-        output = query(uploaded_file.read())
+        image_bytes = uploaded_file.read()
+        output = query(image_bytes)
         st.write("Extracted Text:")
         st.write(output)
+
+    st.subheader("Select a Sample Image:")
+    for index, image_path in enumerate(sample_images):
+        col = st.columns(2)
+        with col[0]:
+            image = Image.open(image_path)
+            st.image(image, use_column_width=True)
+            if st.button(f"Use Image {index+1}", key=f"use_image_{index}"):
+                with open(image_path, "rb") as f:
+                    image_bytes = f.read()
+                output = query(image_bytes)
+                st.write("Extracted Text:")
+                st.write(output)
+
+    
 
 if __name__ == "__main__":
     main()
