1e3741919af71468e3e2c9d4e1f4fb4c29f767cd
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-08 21:13:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/to-be/donut-base-finetuned-invoices"
+headers = {"Authorization": "Bearer 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"}
+
+def query(file):
+    response = requests.post(API_URL, headers=headers, data=file)
+    return response.text
+
+def main():
+    st.title("Handwritten Form Text Extraction")
+    st.write("Upload an image of a handwritten form to extract the text.")
+    
+    uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+    
+    if uploaded_file is not None:
+        output = query(uploaded_file.read())
+        st.write("Extracted Text:")
+        st.write(output)
+
+if __name__ == "__main__":
+    main()
